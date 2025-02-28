93018ce0bcb45fb540cdbca4b594748bebbfbde1
Neithan_bi <igurutejav513@gmail.com>
2025-02-28 09:22:10 +0530

    message

--- Java Files/Test_Runs/car_details.java
+++ Java Files/Test_Runs/car_details.java
@@ -1,6 +1,6 @@
 class car_details {
     public static void main(String[] args) {
         car c = new car(214, 10, 2000000, "M325", "Power");
-        c.display();
+        c.displayDetails();
     }
 }
