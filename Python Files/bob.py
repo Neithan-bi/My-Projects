499fe04663958aaec4a56b406b864a5a4c222b97
Irrvichetty-Tejavardhan <tejavardhan513@gmail.com>
2025-08-11 08:16:46 +0530

    nig

---
+++ Python Files/bob.py
@@ -0,0 +1,65 @@
+a = int(input("Enter value for a : "))
+b = int(input("Enter value for b : "))
+print("Addition of a & b ",a+b)
+print("Subtraction of a & b ",a-b)
+print("Multiplication of a & b ",a*b)
+print("Division of a & b ",a/b)
+print("Modulus of a & b ",a%b)
+print("Floor Division of a & b ",a//b)
+print("The two numbers are {} and {}".format(a,b))
+print("Yogurt\ngurt:YO")
+print("Sup\tBoy")
+#yoyoyoyo
+'''Yoyoyoy'''
+print(a&b)
+print(~a)
+print(a|b)
+print(a<<1)
+print(a>>1)
+print(a==b)
+print(a>10)
+print(a<10)
+print(a!=5)
+print(a>=0)
+print(a<=0)
+print(divmod(a,b))
+print(abs(a))
+print(abs(b))
+g =2.242442
+print(round(g))
+print(isinstance(a,float))
+x = 3+4j
+print(x.real)
+print(x.imag)
+print(x.conjugate)
+print(type(x))
+s="Hello,niggy"
+print(s)
+l="leo"
+h="jheo"
+print(l+""+h)
+print(s*2)
+print(len(s))
+print(s[0],s[-1])
+print(s[1:4])
+print(s.upper(),s.lower())
+print("e"in s)
+print(s.replace("Hello","Kigga"))
+p="yo,gurt,pizza"
+print(s.split(","))
+fruits = ['apple','bannana','eieieie']
+print(",".join(fruits))
+u= "Hel    LO"
+print(s.replace("",""))
+print(s.count("a"))
+print(s.find("niggy"))
+print(s.startswith("Hello"),s.endswith("niggy"))
+j="Yogurt, \"Gurt : Yo!\""
+print(j)
+m = '''kjasbgj
+agsjkbgjasbg
+kjasbgka'''
+print(m)
+print(a.isdigit())
+print(s[::-1])
+
---
+++ Python Files/bob2.py
@@ -0,0 +1,18 @@
+num = int(input("Enter value for num : "))
+sq = num**2
+print("Square of the num is ",sq)
+
+
+a,b = 10,20
+a,b = b,a
+print("After swapping: a =",a,"b = ",b)
+
+c,d = 10,20
+ar = c * d
+at = 0.5 * c * d
+print("Area of Rectangle ",c*d)
+print("Area of Triangle ",0.5*c*d)
+print("Area of Rectangle ",ar)
+print("Area of Triangle ", at)
+
+
